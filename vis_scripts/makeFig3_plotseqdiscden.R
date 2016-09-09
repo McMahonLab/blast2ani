0f424942a0f8db3931bd27dcc8471a2a3c20dd77
Sarah Stevens <sstevens2@wisc.edu>
2016-09-09 10:34:46 -0500

    Updated the vis scripts to the most recent versions

--- vis_scripts/makeFig3_plotseqdiscden.R
+++ vis_scripts/makeFig3_plotseqdiscden.R
@@ -11,8 +11,8 @@ if (length(args)>1) {
   # default output file
   outname = args[1]
 } else {
-  print("Using default outputname 'DATE_Figure-S1_seqdiscplots' both eps and pdf, you can specify a name as the first argument")
-  outname=paste0(Sys.Date(),'_Figure-S1_seqdiscplots')
+  print("Using default outputname 'DATE_Fig3_seqdiscplots' both eps and pdf, you can specify a name as the first argument")
+  outname=paste0(Sys.Date(),'_Fig3_seqdiscplots')
 }
 
 # Metadata for colors
@@ -28,6 +28,7 @@ meta_sub <- metadata %>%
 jColors <- metadata$Color
 names(jColors) <- metadata$SAG_lake
 
+
 #Function that controls the different aspects of the plot
 mydenplotfunc<- function(blastdf){
   return(blastdf  %>% 
@@ -39,27 +40,29 @@ mydenplotfunc<- function(blastdf){
            theme(axis.title=element_text(size=20,face="bold"),legend.title=element_text(size=15), legend.text= element_text(size=15),axis.text = element_text(size=15),axis.text.x  = element_text(angle=90, vjust=0.5)))
 }
 
-
 # Forming each panel (all same using function above)
-# Forming each panel (all same using function above)
-LD12_ME_df <- read.delim('LD12/PTXW.len150-vs-LD12_norrna_short.blast.len200.bbh.keepAll.bbh.onlyME', header = FALSE, sep = '\t',col.names=c('Read', 'Contig', 'PID', 'aln_len','mismatches','gaps','q_start','q_end','s_start','s_end', 'evalue','bitscore','SAG'))
+LD12_ME_df <- read.delim('LD12/PTXW.len150-vs-LD12_norrna_short.blast.len200.bbh.onlyME', header = FALSE, sep = '\t',col.names=c('Read', 'Contig', 'PID', 'aln_len','mismatches','gaps','q_start','q_end','s_start','s_end', 'evalue','bitscore','SAG'))
 LD12_ME_df <- left_join(LD12_ME_df,meta_sub,by = c("SAG"="Genome.name"))
 LD12_ME <- mydenplotfunc(LD12_ME_df)+ggtitle("LD12-Mendota")
 
-acI_ME_df <- read.delim('acI/PTXW.len150-vs-acI_norrna.fna_short.blast.len200.bbh.keepAll.bbh.onlyME', header = FALSE, sep = '\t',col.names=c('Read', 'Contig', 'PID', 'aln_len','mismatches','gaps','q_start','q_end','s_start','s_end', 'evalue','bitscore','SAG'))
+FW_ME_df <- read.delim('FWset/PTXW.len150-vs-FWset_norrna_short.blast.len200.bbh.onlyME', header = FALSE, sep = '\t',col.names=c('Read', 'Contig', 'PID', 'aln_len','mismatches','gaps','q_start','q_end','s_start','s_end', 'evalue','bitscore','SAG'))
+FW_ME_df <- left_join(FW_ME_df,meta_sub,by = c("SAG"="Genome.name"))
+FW_ME <- mydenplotfunc(FW_ME_df)+ggtitle("other-SAGs-Mendota")
+
+acI_ME_df <- read.delim('acI/PTXW.len150-vs-acI_norrna.fna_short.blast.len200.bbh.onlyME', header = FALSE, sep = '\t',col.names=c('Read', 'Contig', 'PID', 'aln_len','mismatches','gaps','q_start','q_end','s_start','s_end', 'evalue','bitscore','SAG'))
 acI_ME_df <- left_join(acI_ME_df,meta_sub,by = c("SAG"="Genome.name"))
 acI_ME <- mydenplotfunc(acI_ME_df)+ggtitle("acI-Mendota")
 
-LD12_nonME_df <- read.delim('LD12/PTXW.len150-vs-LD12_norrna_short.blast.len200.bbh.keepAll.bbh.nonME', header = FALSE, sep = '\t',col.names=c('Read', 'Contig', 'PID', 'aln_len','mismatches','gaps','q_start','q_end','s_start','s_end', 'evalue','bitscore','SAG'))
+LD12_nonME_df <- read.delim('LD12/PTXW.len150-vs-LD12_norrna_short.blast.len200.bbh.nonME', header = FALSE, sep = '\t',col.names=c('Read', 'Contig', 'PID', 'aln_len','mismatches','gaps','q_start','q_end','s_start','s_end', 'evalue','bitscore','SAG'))
 LD12_nonME_df <- left_join(LD12_nonME_df,meta_sub,by = c("SAG"="Genome.name"))
 LD12_nonME <- mydenplotfunc(LD12_nonME_df)+ggtitle("LD12-non-Mendota")
 
-acI_nonME_df <- read.delim('acI/PTXW.len150-vs-acI_norrna.fna_short.blast.len200.bbh.keepAll.bbh.nonME', header = FALSE, sep = '\t',col.names=c('Read', 'Contig', 'PID', 'aln_len','mismatches','gaps','q_start','q_end','s_start','s_end', 'evalue','bitscore','SAG'))
+acI_nonME_df <- read.delim('acI/PTXW.len150-vs-acI_norrna.fna_short.blast.len200.bbh.nonME', header = FALSE, sep = '\t',col.names=c('Read', 'Contig', 'PID', 'aln_len','mismatches','gaps','q_start','q_end','s_start','s_end', 'evalue','bitscore','SAG'))
 acI_nonME_df <- left_join(acI_nonME_df,meta_sub,by = c("SAG"="Genome.name"))
 acI_nonME <- mydenplotfunc(acI_nonME_df)+ggtitle("acI-non-Mendota")
 
 ## Putting plots together and saving them
-all_plots <- plot_grid(acI_ME,LD12_ME, acI_nonME, LD12_nonME, labels=c("A", "B", "C", "D"), ncol = 2, nrow = 2)
-save_plot(paste0(outname,'.eps'), all_plots, base_height = 8, base_width = 12)
-save_plot(paste0(outname,'.pdf'), all_plots, base_height = 8, base_width = 12)
+all_plots <- plot_grid(acI_ME,LD12_ME, FW_ME, acI_nonME, LD12_nonME, labels=c("A", "B", "C", "D", "E"), ncol = 3, nrow = 2, align='hv')
+save_plot(paste0(outname,'.eps'), all_plots, base_height = 8, base_width = 18)
+save_plot(paste0(outname,'.pdf'), all_plots, base_height = 8, base_width = 18)
 
